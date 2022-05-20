d59fa79e54482d6086d38c9aab976afed5d7df85
Damian Cejas <damianfcejas@gmail.com>
2022-05-19 21:01:59 -0300

    ExperienciaLaboral y Educacion de prueba comiteados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { HyssComponent } from './components/hyss/hyss.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
     ExperienciaComponent,
     EducacionComponent,
     HyssComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
